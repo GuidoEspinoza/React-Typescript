bdef89929ccd81a409885dc9941f734f686de2a6
Guido Espinoza <guidoespinozac@gmail.com>
2024-07-01 20:20:36 -0400

    estilado timer

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,15 @@ function App() {
   return (
     <>
       <h1>React + Typescript</h1>
-      <hr />
-      <h2>useState</h2>
-      <Counter />
-      <Usuario/>
-      <hr />
-      <h2>useEffect</h2>
-      <TimerPadre />
+      <div className="d-flex flex-column border mt-5 p-4">
+        <h2>useState</h2>
+        <Counter />
+        <Usuario />
+      </div>
+      <div className="d-flex flex-column border mt-5 p-4">
+        <h2>useEffect</h2>
+        <TimerPadre />
+      </div>
     </>
   )
 }
--- src/components/TimerPadre.tsx
+++ src/components/TimerPadre.tsx
@@ -5,22 +5,21 @@ export const TimerPadre = () => {
     const [milisegundos, setMilisegundos] = useState(1000);
 
     return (
-        <>
+        <div className="d-flex flex-column mt-5 border p-4 row-gap-4 w-50">
             <span>Milisegundos {milisegundos}</span>
-            <br />
             <button
-                className="btn btn-outline-success"
+                className="btn btn-outline-success w-25"
                 onClick={() => setMilisegundos(1000)}
             >
                 1000
             </button>
             <button
-                className="btn btn-outline-success"
+                className="btn btn-outline-success w-25"
                 onClick={() => setMilisegundos(2000)}
             >
                 2000
             </button>
             <Timer milisegundos={milisegundos} />
-        </>
+        </div>
     );
 };
